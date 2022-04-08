39aabbbd19157c5fab355f96617488ba2d87cc3e
Sayantan Chaudhuri <sayantanberhampore@gmail.com>
2022-04-08 16:14:12 +0530

    Initial Commit

---
+++ apps/shell/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: 'locations',
+          loadChildren: () =>
+            import('locations/Module').then((m) => m.RemoteEntryModule),
+        },
+        {
+          path: 'menu',
+          loadChildren: () =>
+            import('menu/Module').then((m) => m.RemoteEntryModule),
+        },
+        {
+          path: 'order',
+          loadChildren: () =>
+            import('order/Module').then((m) => m.RemoteEntryModule),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
